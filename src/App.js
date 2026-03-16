6e7243b07f1f5e5551c8a05672ea856bd5a7f8a0
unknown <abdulkalamjee2021@gmail.com>
2026-03-16 12:42:45 +0530

    fixed layout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Header from './components/header';
 import MainContent from './components/mainContent';
 import Footer  from './components/footer';
+
 function App() {
  
   return (
-    <>
-      <Header/>
-      <MainContent/>
-      <Footer/>
-    </>
+    <div className="page">
+      <Header />
+      <main className="content">
+          <MainContent />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
